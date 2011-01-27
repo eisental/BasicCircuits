564fbe9ae8b2fe3705c189b84ce30c3861105787
Tal Eisenberg <eisental@gmail.com>
2011-01-27 04:42:33 +0100

    Added counter sign arguments min, max, and direction

--- src/main/java/org/tal/basiccircuits/counter.java
+++ src/main/java/org/tal/basiccircuits/counter.java
@@ -11,21 +11,37 @@ public class counter extends Circuit {
     private static final int inputPin = 0;
     private static final int resetPin = 1;
 
-    int count = 0;
+    int min;
+    int max;
+    int direction;
+    int count;
+    boolean updown = false;
 
     @Override
     public void inputChange(int inIdx, boolean on) {
         if (inIdx==inputPin) {
             if (on) { // high from low
-                count++;
-                if (hasDebuggers()) debug("Count incremented to " + count);
+                if (direction==1 && count>=max) {
+                    if (updown) {
+                        direction = -1;
+                        count = max-1;
+                    } else count = min;
+                } else if (direction==-1 && count<=min) {
+                    if (updown) {
+                        direction = 1;
+                        count = min+1;
+                    } else count = max;
+                } else count+=direction;
+
+                if (hasDebuggers()) debug("set to " + count);
                 this.sendInt(0, outputs.length, count);
             }
         } else if (inIdx==resetPin) {
             if (on) { // high from low
-
-                count = 0;
-                if (hasDebuggers()) debug("Count reset to 0");
+                if (direction==-1 && !updown )
+                    count = max;
+                else count = min;
+                if (hasDebuggers()) debug("Count reset to " + count);
                 this.sendInt(0, outputs.length, count);
             }
         }
@@ -38,14 +54,76 @@ public class counter extends Circuit {
             error(player, "Expecting at least 1 input.");
             return false;
         }
-        if (args.length>0) {
+
+        if (args.length==0) {
+            min = 0;
+            max = (int)Math.pow(2, inputs.length);
+            direction = 1;
+        } else if (args.length==1) {
+            // just max
             try {
-                count = Integer.decode(args[0]);
+                max = Integer.decode(args[0]);
             } catch (NumberFormatException ne) {
-                error(player, "Invalid count init argument: " + args[0]);
+                error(player, "Bad max argument: " + args[0]);
+                return false;
+            }
+
+            min = 0;
+            direction = 1;
+        }  else if (args.length==2) {
+            // min and max
+            try {
+                min = Integer.decode(args[0]);
+            } catch (NumberFormatException ne) {
+                error(player, "Bad min argument: " + args[0]);
+                return false;
+            }
+
+            try {
+                max = Integer.decode(args[1]);
+            } catch (NumberFormatException ne) {
+                error(player, "Bad max argument: " + args[1]);
+                return false;
+            }
+
+            if (min>max) {
+                direction = -1;
+                int t = max;
+                max = min;
+                min = t;
+            } else direction = 1;
+        } else if (args.length==3) {
+            // min max and direction
+            try {
+                min = Integer.decode(args[0]);
+            } catch (NumberFormatException ne) {
+                error(player, "Bad min argument: " + args[0]);
+                return false;
+            }
+            try {
+                max = Integer.decode(args[1]);
+            } catch (NumberFormatException ne) {
+                error(player, "Bad max argument: " + args[1]);
+                return false;
+            }
+
+            try {
+                if (args[2].equalsIgnoreCase("up"))
+                    direction = 1;
+                else if (args[2].equalsIgnoreCase("down"))
+                    direction = -1;
+                else if (args[2].equalsIgnoreCase("updown")) {
+                    direction = 1;
+                    updown = true;
+                }
+            } catch (NumberFormatException ne) {
+                error(player, "Bad direction argument: " + args[2]);
                 return false;
             }
         }
+
+        if (direction == 1) count = min;
+        else count = max;
 
         return true;
     }
