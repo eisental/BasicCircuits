342691479a9a337f537ec533c434552fd69af5c7
Tal Eisenberg <eisental@gmail.com>
2011-01-28 21:53:41 +0100

    new flipflop reset mode

--- src/main/java/org/tal/basiccircuits/flipflop.java
+++ src/main/java/org/tal/basiccircuits/flipflop.java
@@ -2,27 +2,39 @@ package org.tal.basiccircuits;
 
 import org.bukkit.entity.Player;
 import org.tal.redstonechips.circuit.Circuit;
+import org.tal.redstonechips.util.BitSet7;
 
 /**
  *
  * @author Tal Eisenberg
  */
 public class flipflop extends Circuit {
+    private boolean resetPinMode = false;
+    private BitSet7 resetBitSet = new BitSet7();
 
     @Override
     public void inputChange(int inIdx, boolean newLevel) {
         if (newLevel) {
-            this.sendOutput(inIdx, !outputBits.get(inIdx));
+            if (resetPinMode) {
+                if (inIdx == 0) { // reset
+                    this.sendBitSet(resetBitSet);
+                } else {
+                    this.sendOutput(inIdx-1, !outputBits.get(inIdx-1));
+                }
+            } else this.sendOutput(inIdx, !outputBits.get(inIdx));
         }
 
     }
 
     @Override
     public boolean init(Player player, String[] args) {
-        if (outputs.length!=inputs.length) {
-            error(player, "Expecting the same number of inputs and outputs.");
+        if (outputs.length!=inputs.length && inputs.length!=outputs.length+1) {
+            error(player, "Expecting the same number of inputs and outputs or one extra input reset pin.");
             return false;
-        } else return true;
+        }
+
+        resetPinMode = (inputs.length==outputs.length+1);
+        return true;
     }
 
 }
