bff018c1bdb00929467f5901222d8baaf2a015b4
Matthew Peychich <mpeychich@goldstar.com>
2011-01-28 19:29:51 -0500

    adding a simple set, reset NOR (srnor) circuit.

---
+++ src/main/java/org/tal/basiccircuits/srnor.java
@@ -0,0 +1,30 @@
+package org.tal.basiccircuits;
+
+import org.bukkit.entity.Player;
+import org.tal.redstonechips.circuit.Circuit;
+
+/**
+ *
+ * @author Matthew Peychich
+ */
+public class srnor extends Circuit {
+
+    @Override
+    public void inputChange(int inIdx, boolean newLevel) {
+      for(int i = 0; i < inputs.length; i++){
+        sendOutput(i, true);
+      }
+      sendOutput(inIdx, false);
+    }
+
+    @Override
+    protected boolean init(Player player, String[] args) {
+        if (inputs.length!=outputs.length) {
+            error(player, "Expecting the same number of inputs and outputs.");
+            return false;
+        }
+
+        return true;
+    }
+
+}
