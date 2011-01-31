4e886f33bace86cbfc4f35932c0dc9d0f823659a
Tal Eisenberg <eisental@gmail.com>
2011-01-31 14:42:59 +0100

    Added pulse argument for positive, negative and double edge-triggering

--- src/main/java/org/tal/basiccircuits/pulse.java
+++ src/main/java/org/tal/basiccircuits/pulse.java
@@ -9,26 +9,18 @@ import org.tal.redstonechips.util.UnitParser;
  * @author Tal Eisenberg
  */
 public class pulse extends Circuit {
-    long interval;
+    enum EdgeTriggering { positive, negative, doubleEdge };
+
+    EdgeTriggering trigger = EdgeTriggering.positive;
+
+    private long interval;
 
     @Override
     public void inputChange(final int inIdx, boolean high) {
-        if (high) {
-            sendOutput(inIdx, true);
-            if (interval==0) {
-                sendOutput(inIdx, false);
-            } else {
-                new Thread() {
-                    @Override
-                    public void run() {
-                        try {
-                            sleep(interval);
-                        } catch (InterruptedException ex) {}
-                        sendOutput(inIdx, false);
-                    }
-                }.start();
-            }
-        }
+        if (high && (trigger==EdgeTriggering.positive || trigger==EdgeTriggering.doubleEdge))
+            pulse(inIdx);
+        else if (!high && (trigger==EdgeTriggering.negative || trigger==EdgeTriggering.doubleEdge))
+            pulse(inIdx);
 
     }
 
@@ -44,9 +36,36 @@ public class pulse extends Circuit {
         }
 
         if (args.length==0) interval = 1000; // 1 sec default
-        else interval = Math.round(UnitParser.parse(args[0]));
+        if (args.length>=1)
+            interval = Math.round(UnitParser.parse(args[0]));
+        if (args.length>=2) {
+            if (args.length==2) {
+                try {
+                    trigger = EdgeTriggering.valueOf(args[1]);
+                } catch (IllegalArgumentException ie) {
+                    error(player, "Bad trigger argument: " + args[1]);
+                }
+            }
+        }
 
         return true;
     }
 
+    private void pulse(final int inIdx) {
+        sendOutput(inIdx, true);
+        if (interval==0) {
+            sendOutput(inIdx, false);
+        } else {
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        sleep(interval);
+                    } catch (InterruptedException ex) {}
+                    sendOutput(inIdx, false);
+                }
+            }.start();
+        }
+
+    }
 }
