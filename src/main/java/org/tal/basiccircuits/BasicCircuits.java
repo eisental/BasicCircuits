f28ba0652c20d59086f68f2245ba43dc1dce7d49
Tal Eisenberg <eisental@gmail.com>
2011-02-04 17:12:38 +0100

    Necessary changes for using the new CircuitLibrary superclass

--- src/main/java/org/tal/basiccircuits/BasicCircuits.java
+++ src/main/java/org/tal/basiccircuits/BasicCircuits.java
@@ -7,18 +7,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.tal.redstonechips.CircuitLibrary;
 import org.tal.redstonechips.RedstoneChips;
 import org.tal.redstonechips.util.TargetBlock;
 
@@ -26,51 +23,26 @@ import org.tal.redstonechips.util.TargetBlock;
  *
  * @author Tal Eisenberg
  */
-public class BasicCircuits extends JavaPlugin {
-    private RedstoneChips rc;
-
+public class BasicCircuits extends CircuitLibrary {
 
     public static List<transmitter> transmitters = new ArrayList<transmitter>();
     public static List<receiver> receivers = new ArrayList<receiver>();
     public static Map<Location, terminal> terminals = new HashMap<Location, terminal>();
 
-    public static final String rcName = "RedstoneChips";
-    static final Logger log = Logger.getLogger("Minecraft");
-
     public BasicCircuits(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
         super(pluginLoader, instance, desc, folder, plugin, cLoader);
+
+        RedstoneChips.addCircuitLibrary(this);
     }
 
     @Override
-    public void onDisable() {
-        log.info(getDescription().getName() + " " + getDescription().getVersion() + " disabled.");
-        if (rc!=null)
-            rc.removeCircuitClasses(adder.class, and.class, clock.class, counter.class, demultiplexer.class, divider.class, flipflop.class,
-                    multiplexer.class, multiplier.class, or.class, pisoregister.class, print.class, random.class, receiver.class,
-                    shiftregister.class, transmitter.class, xor.class, decoder.class, encoder.class, pixel.class, pulse.class, not.class,
-                    synth.class, srnor.class, terminal.class, router.class, decadecounter.class);
+    public Class[] getCircuitClasses() {
+        return new Class[] { adder.class, and.class, clock.class, counter.class, demultiplexer.class, divider.class, flipflop.class,
+                multiplexer.class, multiplier.class, or.class, pisoregister.class, print.class, random.class, receiver.class,
+                shiftregister.class, transmitter.class, xor.class, decoder.class, encoder.class, pixel.class, pulse.class, not.class,
+                synth.class, srnor.class, terminal.class, router.class, decadecounter.class };
     }
-
-    @Override
-    public void onEnable() {
-        Plugin p = getServer().getPluginManager().getPlugin(rcName);
-        if (p==null) {
-            log.warning(getDescription().getName() + " " + getDescription().getVersion() + ": Required plugin " + rcName + " is missing.");
-        }
-
-        rc = (RedstoneChips)p;
-        try {
-            rc.addCircuitClasses(adder.class, and.class, clock.class, counter.class, demultiplexer.class, divider.class, flipflop.class,
-                    multiplexer.class, multiplier.class, or.class, pisoregister.class, print.class, random.class, receiver.class,
-                    shiftregister.class, transmitter.class, xor.class, decoder.class, encoder.class, pixel.class, pulse.class, not.class, 
-                    synth.class, srnor.class, terminal.class, router.class, decadecounter.class);
-        } catch (NoClassDefFoundError ncde) {
-            log.log(Level.SEVERE, getDescription().getName() + ": Can't find RedstoneChips plugin. Filename or version mismatch.");
-        }
-        
-        log.info(getDescription().getName() + " " + getDescription().getVersion() + " circuit package enabled.");
-    }
-
+    
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (cmd.getName().equalsIgnoreCase("redchips-channels")) {
@@ -78,11 +50,11 @@ public class BasicCircuits extends JavaPlugin {
             for (transmitter t : transmitters) channels.add(t.getChannel());
             for (receiver r : receivers) channels.add(r.getChannel());
             if (channels.isEmpty()) {
-                sender.sendMessage(rc.getPrefsManager().getInfoColor() + "There are no registered channels.");
+                sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + "There are no registered channels.");
             } else {
                 sender.sendMessage("");
-                sender.sendMessage(rc.getPrefsManager().getInfoColor() + "Currently used broadcast channels:");
-                sender.sendMessage(rc.getPrefsManager().getInfoColor() + "------------------------------");
+                sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + "Currently used broadcast channels:");
+                sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + "------------------------------");
                 String list = "";
                 ChatColor color = ChatColor.WHITE;
                 for (String channel : channels) {
@@ -96,8 +68,8 @@ public class BasicCircuits extends JavaPlugin {
                     else color = ChatColor.WHITE;
                 }
                 if (!list.isEmpty()) sender.sendMessage(list.substring(0, list.length()-2));
-                sender.sendMessage(rc.getPrefsManager().getInfoColor() + "Used by " + transmitters.size() + " transmitter(s) and " + receivers.size() + " receiver(s).");
-                sender.sendMessage(rc.getPrefsManager().getInfoColor() + "------------------------------");
+                sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + "Used by " + transmitters.size() + " transmitter(s) and " + receivers.size() + " receiver(s).");
+                sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + "------------------------------");
                 sender.sendMessage("");
             }
             
@@ -110,7 +82,7 @@ public class BasicCircuits extends JavaPlugin {
             TargetBlock b = new TargetBlock(player);
             terminal t = terminals.get(b.getTargetBlock().getLocation());
             if (t==null) {
-                player.sendMessage(rc.getPrefsManager().getErrorColor() + "You must point towards a terminal screen (a terminal circuit's interface block) to type anything.");
+                player.sendMessage(redstoneChips.getPrefsManager().getErrorColor() + "You must point towards a terminal screen (a terminal circuit's interface block) to type anything.");
             } else {
                 t.type(args, player);
             }
