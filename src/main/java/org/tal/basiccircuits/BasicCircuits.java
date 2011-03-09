fe609639a274bbcd8892396017758ffbde04f0ef
Tal Eisenberg <eisental@gmail.com>
2011-03-10 00:35:26 +0100

    Updated to work with RC0.84 new circuit library loading mechanism

--- src/main/java/org/tal/basiccircuits/BasicCircuits.java
+++ src/main/java/org/tal/basiccircuits/BasicCircuits.java
@@ -1,41 +1,26 @@
 package org.tal.basiccircuits;
 
-import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.tal.redstonechips.RedstoneChips;
 import org.tal.redstonechips.circuit.CircuitLibrary;
 
 /**
  *
  * @author Tal Eisenberg
  */
-public class BasicCircuits extends JavaPlugin {
-    public static final Logger logger = Logger.getLogger("Minecraft");
-
-    public BasicCircuits() {
-        new CircuitLibrary() {
-
-            @Override
-            public Class[] getCircuitClasses() {
-                return new Class[] { adder.class, and.class, clock.class, counter.class, demultiplexer.class, divider.class, flipflop.class,
-                        multiplexer.class, multiplier.class, or.class, pisoregister.class, print.class, random.class, receiver.class,
-                        shiftregister.class, transmitter.class, xor.class, decoder.class, encoder.class, pixel.class, pulse.class, not.class,
-                        synth.class, srnor.class, terminal.class, router.class, ringcounter.class, iptransmitter.class, ipreceiver.class,
-                        comparator.class, delay.class, repeater.class, nand.class, nor.class, xnor.class, segdriver.class };
-            }
-
-            @Override
-            public void onRedstoneChipsEnable() {
-                // add a new pref key for iptransmitter.
-                redstoneChips.getPrefsManager().registerCircuitPreference(iptransmitter.class, "ports", "25600..25699");
-            }
-        };
+public class BasicCircuits extends CircuitLibrary {
+    @Override
+    public Class[] getCircuitClasses() {
+        return new Class[] { adder.class, and.class, clock.class, counter.class, demultiplexer.class, divider.class, flipflop.class,
+                multiplexer.class, multiplier.class, or.class, pisoregister.class, print.class, random.class, receiver.class,
+                shiftregister.class, transmitter.class, xor.class, decoder.class, encoder.class, pixel.class, pulse.class, not.class,
+                synth.class, srnor.class, terminal.class, router.class, ringcounter.class, iptransmitter.class, ipreceiver.class,
+                comparator.class, delay.class, repeater.class, nand.class, nor.class, xnor.class, segdriver.class };
     }
 
     @Override
-    public void onDisable() {}
-
-    @Override
-    public void onEnable() {
+    public void onRedstoneChipsEnable(RedstoneChips instance) {
+        
+        // add a new pref key for iptransmitter.
+        instance.getPrefsManager().registerCircuitPreference(iptransmitter.class, "ports", "25600..25699");
     }
-    
 }
