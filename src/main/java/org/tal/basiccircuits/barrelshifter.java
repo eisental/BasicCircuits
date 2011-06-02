8ec2d2fdd302b4a972ae214df2f9eab30c14ca15
Zynine <Zynine@hotmail.com>
2011-06-01 19:09:35 -0700

    should fix barrel shifter, that is, if inputs and outputs will work

--- src/main/java/org/tal/basiccircuits/barrelshifter.java
+++ src/main/java/org/tal/basiccircuits/barrelshifter.java
@@ -15,7 +15,7 @@ public class barrelshifter extends Circuit {
     public void inputChange(int inIdx, boolean state) {
         if (inputBits.get(0)) {	//First Input is Clock
             if (outputBits.length() == 0) {
-                barrel(inputBits.get(2), inputBits.get(3, inputs.length));
+                barrel(inputBits.get(2), inputs);
             } else {
                 barrel(inputBits.get(2), outputBits);
             }
@@ -28,28 +28,18 @@ public class barrelshifter extends Circuit {
 	}
     }
 
-    public void barrel(boolean direction, BitSet7 in) { // Left is false or 0 and Right is true or 1
+    public void barrel(boolean direction, int[] in) { // Left is false or 0 and Right is true or 1
  	if (!direction) {
-            for (int i = 0; i < outputs.length; i++) {
-                if (i + 1 < outputs.length) {
-                    outputBits.set(i + 1, in.get(i));
-                } else {
-                    outputBits.set(0, in.get(outputs.length-1));
-                    outputBits.set(outputs.length-1, in.get(outputs.length-2));
-                }
+                  for(int i = 1; i<outputs.length-1; i++) {
+                    output[i] = in[i+2];
+        } 
+                    outputs[0] = in[in.length];
+ 	if (!direction) {
+                  for(int i = outputs.length-1; i>3; i--) {
+                    output[i] = in[i-1];
+        } 
+                    outputs[outputs.length] = in[3];
             }
-        } else {
-            for (int i=0; i < outputs.length; i++) {
-                if (i < outputs.length)
-                    outputBits.set(i, in.get(i+1));
-                else {
-                    outputBits.set(outputs.length-2, in.get(outputs.length-1));
-                    outputBits.set(outputs.length-1, in.get(0));
-                }
-            }
-        }
-    }
-
     @Override
     protected boolean init(CommandSender sender, String[] args) {
         if (outputs.length != inputs.length - 3) {
