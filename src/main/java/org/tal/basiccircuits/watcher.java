cd1344430007f71691bbe42aaf609c69b9c8f21b
John Petska <jpfx1342@gmail.com>
2012-09-25 23:39:11 -0500

    add: added ram watcher chip
    Added the ram watcher chip. The watcher chip takes 1 input and 1 output.
    It also takes 1 or 2 arguments. The first is mandatory and specifies the
    ram to watch, while the 2 specifies the ram address to watch.
    The input functions as a disable/enable pin. If it is off, the chip
    does not function. The output pin will send a 1-tick pulse when the
    specified ram address in the specified ram id is changed. (If no address
    is given, the entire ram is watched.) This is useful for triggering the
    clock pin of a ram-backed display when it's ram-page is determined by
    a ram-backed dregister.

---
+++ src/main/java/org/tal/basiccircuits/watcher.java
@@ -0,0 +1,97 @@
+
+package org.tal.basiccircuits;
+
+import java.io.IOException;
+import org.bukkit.command.CommandSender;
+import org.tal.redstonechips.circuit.Circuit;
+import org.tal.redstonechips.bitset.BitSet7;
+import org.tal.redstonechips.bitset.BitSetUtils;
+import org.tal.redstonechips.memory.Memory;
+import org.tal.redstonechips.memory.Ram;
+import org.tal.redstonechips.memory.RamListener;
+
+/**
+ *
+ */
+public class watcher extends Circuit {
+    private Ram ram;
+    private RamListener ramListener;
+    private BitSet7 ramaddr;
+    
+    private PulseOff pulseOff = new PulseOff();
+    
+    @Override
+    public void inputChange(int inIdx, boolean state) {
+    }
+    
+    class WatcherRamListener implements RamListener {
+        @Override
+        public void dataChanged(Ram ram, BitSet7 address, BitSet7 data) {
+            if (inputBits.get(0) & (ramaddr == null || ramaddr.equals(address))) {
+                sendOutput(0, true);
+                redstoneChips.getServer().getScheduler().scheduleSyncDelayedTask(redstoneChips, pulseOff, 1);
+            }
+        }
+    }
+    
+    @Override
+    protected boolean init(CommandSender sender, String[] args) {
+        if (args.length >= 1) {
+            if (args[0].startsWith("$")) {
+                try {
+                    if (args.length >= 2)
+                        ramaddr = BitSetUtils.intToBitSet(Integer.decode(args[1]));
+                    else
+                        ramaddr = null;
+                    
+                    ram = (Ram)Memory.getMemory(args[0].substring(1), Ram.class);
+                } catch (IllegalArgumentException e) {
+                    error(sender, e.getMessage());
+                } catch (IOException e) {
+                    error(sender, e.getMessage());
+                }
+            } else error(sender, "Invalid argument: " + args[0]);
+        } else {
+            error(sender, "Expected at least one argument, the ram to watch.");
+            return false;
+        }
+        
+        if (inputs.length < 1 || outputs.length < 1) {
+            error(sender, "Expected at least one input and output. Found " + inputs.length + " input(s) and " + outputs.length + " output(s).");
+            return false;
+        }
+        
+        if (sender!=null) resetOutputs();
+        
+        if (ram != null) {
+            ramListener = new WatcherRamListener();
+            ram.addListener(ramListener);
+            
+            if (ramaddr==null)
+                info(sender, "Created ram watcher targeting entirety of $"+ram.getId());
+            else
+                info(sender, "Created ram watcher targeting $"+ram.getId()+"@"+Integer.toHexString(BitSetUtils.bitSetToUnsignedInt(ramaddr, 0, ramaddr.length())));
+        } else {
+            error(sender, "Couldn't find ram to watch.");
+            return false;
+        }
+        return true;
+    }
+    @Override
+    protected void circuitShutdown() {
+        if (ram != null)
+           ram.getListeners().remove(ramListener);
+    }
+    
+    class PulseOff implements Runnable {
+        @Override
+        public void run() {
+            sendOutput(0, false);
+        }
+    }
+    
+    @Override
+    protected boolean isStateless() {
+        return false;
+    }
+}
