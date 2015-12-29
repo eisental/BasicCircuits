a92aa9f5db3a41e600f8b29e95ae1c7caa2f71a8
Scott Squires <squires@pobox.com>
2015-12-29 14:35:08 -0600

    add repeated trigger mode setting to pulse circuit
    The pulse circuit had (most likely) undesired behavior when the Circuit
    was triggered while a pulse was active. The subsequent triggers would
    unconditionally schedule tasks to turn off the signal after the pulse
    length. So if another pulse started after the first pulse ended, but there
    were additional triggers during the initial pulse, then the second pulse
    would turn off early.
    
    This commit adds a "repeated trigger" parameter to the pulse circuit.
    The 3rd parameter is optional and defaults to "legacy" for the original
    behavior. A value of "ignore" causes the circuit to ignore any triggers
    while the pulse is active. A value of "extend" causes the circuit to cancel
    the original task that would turn off the pulse and schedule a new one
    (for the original pulse length).

--- src/main/java/org/redstonechips/basiccircuits/pulse.java
+++ src/main/java/org/redstonechips/basiccircuits/pulse.java
@@ -10,23 +10,22 @@ import org.redstonechips.parsing.UnitParser;
 public class pulse extends Circuit {
 
     public enum EdgeTriggering { positive, negative, doubleEdge };
+    public enum RepeatedTriggerMode { legacy, ignore, extend };
 
     private EdgeTriggering trigger = EdgeTriggering.positive;
+    private RepeatedTriggerMode repeatedTriggerMode = RepeatedTriggerMode.legacy;
 
     private long interval;
     private PulseOff[] pulseOffs;
+    private Integer[] pulseOffIds;
     private long intervalInTicks;
 
     @Override
     public void input(boolean state, final int inIdx) {
         if (state && (trigger==EdgeTriggering.positive || trigger==EdgeTriggering.doubleEdge)) {
-            if (inputlen==outputlen)
-                pulse(inIdx);
-            else pulseSequence();
+            trigger(inIdx);
         } else if (!state && (trigger == EdgeTriggering.negative || trigger == EdgeTriggering.doubleEdge)) {
-            if (inputlen==outputlen)
-                pulse(inIdx);
-            else pulseSequence();
+            trigger(inIdx);
         }
 
     }
@@ -38,7 +37,6 @@ public class pulse extends Circuit {
 
         if (inputlen==0) 
             return error("Expecting at least one input and one output.");
-        
 
         if (args.length==0) interval = 1000; // 1 sec default
         
@@ -51,33 +49,75 @@ public class pulse extends Circuit {
         }
 
         if (args.length>=2) {
-            if (args.length==2) {
-                try {
-                    trigger = EdgeTriggering.valueOf(args[1]);
-                } catch (IllegalArgumentException ie) {
-                    return error("Bad trigger argument: " + args[1]);
-                }
+            try {
+                trigger = EdgeTriggering.valueOf(args[1]);
+            } catch (IllegalArgumentException ie) {
+                return error("Bad trigger argument: " + args[1]);
             }
+        }
+        
+        if (args.length>=3) {
+            try {
+                repeatedTriggerMode = RepeatedTriggerMode.valueOf(args[2]);
+            } catch (IllegalArgumentException e) {
+                return error("Bad repeated trigger mode argument: " + args[2]);
+            }
+        }
+        
+        if (repeatedTriggerMode==RepeatedTriggerMode.extend && inputlen!=outputlen) {
+            return error("Extend mode is incompatible with sequence mode");
         }
 
         if (interval!=0) {
             intervalInTicks = Math.round(interval/50);
+            pulseOffIds = new Integer[outputlen];
 
             pulseOffs = new PulseOff[outputlen];
             for (int i=0; i < outputlen; i++) {
-                pulseOffs[i] = new PulseOff(i);
+                pulseOffs[i] = new PulseOff(i, pulseOffIds);
             }
         }
 
         return this;
     }
-
+    
+    private void trigger(final int idx) {
+        int checkRepeatIdx = 0;
+        
+        if (repeatedTriggerMode==RepeatedTriggerMode.legacy) {
+            pulse(idx);
+        }
+        else {
+            if (inputlen==outputlen) {
+                checkRepeatIdx = idx;
+            }
+            if (repeatedTriggerMode==RepeatedTriggerMode.ignore) {
+                if (pulseOffIds[checkRepeatIdx]==null) {
+                    pulse(idx);
+                }
+            }
+            else if (repeatedTriggerMode==RepeatedTriggerMode.extend) {
+                if (pulseOffIds[checkRepeatIdx]!=null) {
+                    rc.getServer().getScheduler().cancelTask(pulseOffIds[checkRepeatIdx]);
+                    pulseOffIds[checkRepeatIdx] = null;
+                }
+                pulse(idx);
+            }
+        }
+    }
+    
     private void pulse(final int idx) {
+        if (inputlen==outputlen)
+            pulseSingle(idx);
+        else pulseSequence();
+    }
+
+    private void pulseSingle(final int idx) {
         write(true, idx);
         if (interval==0) {
             write(false, idx);
         } else {
-            rc.getServer().getScheduler().scheduleSyncDelayedTask(rc, pulseOffs[idx], intervalInTicks);
+            pulseOffIds[idx] = rc.getServer().getScheduler().scheduleSyncDelayedTask(rc, pulseOffs[idx], intervalInTicks);
         }
 
     }
@@ -90,18 +130,25 @@ public class pulse extends Circuit {
             }
         } else {
             write(true, 0);
-            rc.getServer().getScheduler().scheduleSyncDelayedTask(rc, pulseOffs[0], intervalInTicks);
+            pulseOffIds[0] = rc.getServer().getScheduler().scheduleSyncDelayedTask(rc, pulseOffs[0], intervalInTicks);
         }
     }
 
     class PulseOff implements Runnable {
         int index;
+        Integer[] pulseOffIds;
 
-        public PulseOff(int index) { this.index = index; }
+        public PulseOff(int index, Integer[] pulseOffIds) {
+            this.index = index;
+            this.pulseOffIds = pulseOffIds;
+        }
 
         @Override
         public void run() {
             write(false, index);
+            if (inputlen==outputlen || index==0) {
+                pulseOffIds[index] = null;
+            }
             if (inputlen!=outputlen && pulseOffs.length>index+1) {
                 write(true, index+1);
                 rc.getServer().getScheduler().scheduleSyncDelayedTask(rc, pulseOffs[index+1], intervalInTicks);
